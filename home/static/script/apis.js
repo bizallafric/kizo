5e83a94073b5a04e5840ba7bb10cd1f6b48dddc0
John william <quickenloans.personal@gmail.com>
2020-09-28 02:09:23 -0700

    some changes made

--- home/static/script/apis.js
+++ home/static/script/apis.js
@@ -384,7 +384,7 @@ function getAuthState(){
         // redirect user to seller whatsapp account
         else{
             
-            window.location.href="https://wa.me/"+get_seller_num+"/?text=" +get_product_url 
+            window.location.href="https://wa.me/"+get_seller_num+"/?text=" +"http://"+get_product_url +"%0a‎" +"Hello I am interested in product"
             
         }
     }
